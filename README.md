5d38b49d9d8e21b423b201e3f5b50d68a61e2f43
Daluwatumulle <SDALUWA@emory.edu>
2025-02-04 22:05:15 -0500

    my fourth commit?

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # example_project
 
 
-## Structure
+## Structure of the project folders
 
 * `analysis/` is intended to contain all the source R Markdown files and the pdf reports that
 summarise the analysis results for the project.
@@ -11,15 +11,24 @@ a subdirectory
 * `source/` will contain bare scripts (typically containing functions sourced
 by the full analysis files)
 * `Simulation/` will contain scripts to simulate the data for the differeny scenarios
+* `Report/` will contain rmd and pdf file submitted on canvas containing the final report and answers
 
-
+## Running order to reproduce results
+* Open up the HW1.Rproj file. 
+* To generate the results first run the Simulation.R file in Simulation folder. 
+        **This will load all the libraries and functions first. The run the scenarios using parallelization.
+         It will save .RDA files for each scenario in results folder. After all 18 scenarios are done, 
+         a summary table will also be saved summarizing all the results of the 18 scenarios
+* To get the final report, run the rmd file under reports folder
 
 ## Libraries used
-
 
 Below are the libraries used in analysis
 
 ```
 library(tidyverse)
 library(tictoc)
+library(doParallel)
+library(foreach)
+library(ggplot2)
 ```
--- Source/Functions.R
+++ Source/Functions.R
@@ -1,69 +1,83 @@
+#################### Function to simulate data according to n, beta_treat and sigma2 ##############
+
 get_simdata= function (n, beta_treat, sigma2){
-  if (sigma2==2){
+  if (sigma2==2){ # generate data under normal
     
     X<- rbinom(n, 1, prob = 0.5)
     epsi<- rnorm(n,sqrt(sigma2))
     Y<-1+ beta_treat*X+epsi #is beta0 1?
+    
   }
   
-  else{
+  else{ # generate data under lognormal
+    
     X<- rbinom(n, 1, prob = 0.5)
     epsi<- rlnorm(n, meanlog = 0, sdlog = sqrt(exp(sigma2)))
     Y<-1+ beta_treat*X+epsi
-    
   }
   return(as.data.frame(cbind(Y,X)))
   
 }
 
+
+############################### Function to fit the simple linear regression model #############################
 fit_model= function(simdata){
+  
   model_fit=lm(Y~X, data=simdata)
   
   return(model_fit)
 }
 
-get_estimates= function(model_fit , true_beta ){
-  summary_model=summary(model_fit)
+
+################################# Function to return the necessary estimated #################
+
+get_estimates= function(model_fit ){
+  #estimates dataframe
+  summary_model=summary(model_fit) 
+  #CI for the estimates
   confid_int=confint(model_fit)
+  
   estimates=cbind(coef=summary_model$coefficients[2,1],sd=summary_model$coefficients[2,2],
                   lower=confid_int[2,1],upper=confid_int[2,2])
-  
+  # estimates return beta_hat, sd of beta_hat, lower CI and upper CI respectively
   return(estimates)
   
 }
 
-boot_nonpara_perc_pairs=function(sample,B,alpha){
- n=nrow(sample)
-nonboot_est = rep(0, B) 
-for (i in 1:B) {   
-  nboot.data = sample[sample(c(1:n), n, replace=TRUE),   ]
-  model_fit=lm(Y~X, data=nboot.data)
-  nonboot_est[i] = summary(model_fit)$coefficients[2,1]
-  
-} 
 
-quantile(nonboot_est, probs = c(alpha/2, 1-(alpha/2)))
+########################## function to conduct bootstrap percentile method ##########################
 
-}
 
 boot_nonpara_perc_residuals=function(sample,beta_est,B,alpha=0.05){
+  # calculate the residuals from X Y data
   residuals=sample$Y-sample$X*beta_est
   n=nrow(sample)
-  nonboot_est = rep(0, B) 
+  
+  nonboot_est = rep(0, B)
   for (i in 1:B) {   
+    # sample residuals with replacement
     nboot.res = sample(residuals, n, replace=TRUE)
+    # calculate new y_b as y_b=x*beta_hat+ epsilon_b 
     nboot.data=data.frame(Y_b=sample$X*beta_est+nboot.res,X=sample$X)
+    # fit the model for y_c and X 
     model_fit=lm(Y_b~X, data=nboot.data)
+    #save the coefficient estimate
     nonboot_est[i] = summary(model_fit)$coefficients[2,1]
     
   } 
   
+  # we aren't calculating bias for this. If we want to, then nonboot_mean=mean(nonboot_est)
+  nonboot_sd = sd(nonboot_est) # save the sd of bootstrap sample
+  # calculate the 95% percentile from the bootstrap sample
   boot_perc=quantile(nonboot_est, probs = c(alpha/2, 1-(alpha/2)))
   
-  return(boot_perc)
+  return(c(nonboot_sd,boot_perc))
   
 }
 
+########################## function to conduct bootstrap t method ##########################
+
+# similar as boot_nonpara_perc_residuals function
 boot_nonpara_t_residuals=function(sample,beta_est,B,nt,alpha=0.05){
   residuals=sample$Y-sample$X*beta_est
   n=nrow(sample)
@@ -74,9 +88,11 @@ boot_nonpara_t_residuals=function(sample,beta_est,B,nt,alpha=0.05){
     model_fit=lm(Y_b~X, data=nboot.data)
     nonboot_est[i] = summary(model_fit)$coefficients[2,1]
     
-    nonboot_est2 = rep(NA, nt)
+    nonboot_est2 = rep(NA, nt) # 2nd round of for loops
     for(k in 1:nt){
+      # sample from the nboot.res
       ystar_k = sample(nboot.res, size = n, replace = TRUE)
+      #calculate y_b2= x*beta_est + ystart_k
       nboot.data2=data.frame(Y_b2=sample$X*beta_est+ystar_k,X=sample$X)
       model_fit2=lm(Y_b2~X, data=nboot.data2)
       nonboot_est2[k] = summary(model_fit2)$coefficients[2,1]
